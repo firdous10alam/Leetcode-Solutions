34d35c83eb83e8a90a8b666596aaa9fd5f9339e6
Mohammad Firdous Alam <mdfirdous10alam@gmail.com>
2026-06-21 16:43:28 +0530

    Added Linked List Questions

---
+++ Linked-List/DeleteMiddleAndMerge.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+
+        if(head == null || head.next == null) {
+            return null;
+        }
+        
+        ListNode slow = new ListNode();
+        ListNode fast = new ListNode();
+        ListNode pre = new ListNode();
+
+        slow = head;
+        fast = head;
+
+        while(fast != null && fast.next != null) {
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        pre.next = slow.next;
+        
+        return head;
+        
+    }
+}
